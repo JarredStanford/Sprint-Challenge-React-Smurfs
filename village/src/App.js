ca46f89ec0b1b005264fead3fb77e26275874b5b
unknown <stanj89@gmail.com>
2019-06-14 11:33:07 -0400

    get and post methods functioning

--- village/src/App.js
+++ village/src/App.js
@@ -1,23 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import { Route, Link } from "react-router-dom";
 
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(response => {
+        this.setState({
+          smurfs: response.data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+  //this makes a call to our server and uses the dataset from the response to set our state.
+
+  postSmurf = newSmurf => {
+    axios
+      .post("http://localhost:3333/smurfs", newSmurf)
+      .then(response => {
+        this.setState({
+          smurfs: response.data
+        });
+      })
+      .catch(err => console.log(err));
+  };
+  //this sends our new smurf data as an argument to post to the server. It then sets state on App to update our state with the new smurf added.
+
   render() {
+    console.log(this.state.smurfs);
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm postSmurf={this.postSmurf} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      age: '',
-      height: ''
+      name: "",
+      age: Number(""),
+      height: ""
     };
   }
 
-  addSmurf = event => {
-    event.preventDefault();
+  addSmurf = e => {
+    e.preventDefault();
     // add code to create the smurf using the api
-
+    this.props.postSmurf(this.state);
     this.setState({
-      name: '',
-      age: '',
-      height: ''
+      name: "",
+      age: "",
+      height: ""
     });
-  }
+  };
 
   handleInputChange = e => {
     this.setState({ [e.target.name]: e.target.value });
@@ -40,6 +40,7 @@ class SmurfForm extends Component {
             placeholder="age"
             value={this.state.age}
             name="age"
+            type="number"
           />
           <input
             onChange={this.handleInputChange}
